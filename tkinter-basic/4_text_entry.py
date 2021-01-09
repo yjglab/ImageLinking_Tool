1ae974a5be145616008f9329d05c098e4c19b812
yjglab <yjgdesign@gmail.com>
2021-01-09 20:30:04 +0900

    Text and Entry

---
+++ tkinter-basic/4_text_entry.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+root = Tk()
+root.title("GUI PROGRAM")
+root.geometry("640x480+900+600") # 가로x세로+X+Y
+root.resizable(False, False) # X(너비), Y(높이) 값 변경 불가
+
+txt = Text(root, width=30 ,height=5) # 텍스트 위젯
+txt.pack()
+
+txt.insert(END, "글자를 입력하세요") # 입력 위치. 
+
+e = Entry(root, width=30)
+e.pack()
+e.insert(0, "한 줄만 입력하세요") # 입력 위치
+
+def btncmd():
+    # 내용 출력
+    print(txt.get("1.0", END)) # 1(line 1번째).0(column기준 0번째) 부터 END까지 불러옴
+    print(e.get()) # Entry 값 불러옴
+
+    # 내용 삭제
+    txt.delete("1.0", END)
+    e.delete(0, END)
+    
+btn = Button(root, text="클릭", command=btncmd)
+btn.pack()
+
+root.mainloop()
