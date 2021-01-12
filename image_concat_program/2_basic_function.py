2345694da426ab3c6dbca48476f783bbcfe03eca
yjglab <yjgdesign@gmail.com>
2021-01-13 00:19:48 +0900

    add functions

---
+++ image_concat_program/2_basic_function.py
@@ -0,0 +1,131 @@
+import tkinter.ttk as ttk
+import tkinter.messagebox as msgbox
+from tkinter import *
+from tkinter import filedialog # 서브 모듈. 별도 지정 필요
+
+root = Tk()
+root.title("이미지 연결 도구")
+# root.geometry("640x480+900+600") 
+
+# 파일 추가 
+def add_file():
+    files = filedialog.askopenfilenames(title="이미지 파일 선택", \
+        filetypes=(("PNG 파일", "*.png"), ("모든 파일", "*.*")), \
+            initialdir=r"C:\Users") # r: string 있는 그대로 읽음
+            # 최초에 C:/ 경로를 표시함
+    
+    for file in files:
+        list_file.insert(END, file)    
+
+# 선택 삭제
+def del_file():
+    # reversed() : index 순서를 뒤집은 새로운 값. 원본에는 영향을 미치지 않음.
+    for index in reversed(list_file.curselection()): 
+        list_file.delete(index)
+
+# 저장 경로 (폴더)
+def browse_dest_path():
+    folder_selected = filedialog.askdirectory()
+    if folder_selected == "": # 사용자가 취소 누르면
+        return
+    txt_dest_path.delete(0, END)
+    txt_dest_path.insert(0, folder_selected)
+
+# 시작
+def start():
+    # 각 옵션 값 확인
+    
+    if list_file.size() == 0:
+        msgbox.showwarning("경고", "이미지 파일을 추가하세요")
+        return
+    # 저장 경로 확인
+    if len(txt_dest_path.get()) == 0:
+        msgbox.showwarning("경고", "저장 경로를 선택하세요")
+        return
+
+# 파일 프레임 (파일 추가, 파일 삭제)
+file_frame = Frame(root)
+file_frame.pack(fill="x", padx=5, pady=5)
+
+btn_add_file = Button(file_frame, padx=5, pady=5, width=12, text="파일 추가", command=add_file)
+btn_add_file.pack(side="left")
+
+btn_del_file = Button(file_frame, padx=5, pady=5, width=12, text="선택 삭제", command=del_file)
+btn_del_file.pack(side="right")
+
+# 리스트 프레임
+list_frame = Frame(root)
+list_frame.pack(fill="both", padx=5, pady=5)
+
+scrollbar = Scrollbar(list_frame)
+scrollbar.pack(side="right", fill="y")
+
+list_file = Listbox(list_frame, selectmode="extended", height=15, yscrollcommand=scrollbar.set)
+list_file.pack(side="left", fill="both", expand=True)
+scrollbar.config(command=list_file.yview)
+
+# 저장 경로 프레임
+path_frame = LabelFrame(root, text="저장 경로")
+path_frame.pack(fill="x", padx=5, pady=5, ipady=5)
+
+txt_dest_path = Entry(path_frame)
+txt_dest_path.pack(side="left", fill="x", expand=True, padx=5, pady=5, ipady=4) # ipady(inner padding y)
+
+btn_dest_path = Button(path_frame, text="가져오기", width=10, command=browse_dest_path)
+btn_dest_path.pack(side="right", padx=5, pady=5)
+
+# 옵션 프레임
+frame_option = LabelFrame(root, text="옵션")
+frame_option.pack(padx=5, pady=5, ipady=5)
+
+# 기로 넓이 옵션
+# *가로 넓이 레이블
+lbl_width = Label(frame_option, text="폭", width=8)
+lbl_width.pack(side="left", padx=5, pady=5)
+# *가로 넓이 콤보
+opt_width = ["원본 유지", "1024", "800", "640"]
+cmb_width = ttk.Combobox(frame_option, state="readonly", values=opt_width, width=10)
+cmb_width.current(0)
+cmb_width.pack(side="left", padx=5, pady=5)
+
+# 간격 옵션
+# *간격 옵션 레이블
+lbl_space = Label(frame_option, text="간격", width=8)
+lbl_space.pack(side="left", padx=5, pady=5)
+# *간격 옵션 콤보
+opt_space = ["없음", "좁게", "보통", "넓게"]
+cmb_space = ttk.Combobox(frame_option, state="readonly", values=opt_space, width=10)
+cmb_space.current(0)
+cmb_space.pack(side="left", padx=5, pady=5)
+
+# 파일 포맷 옵션
+# *파일 포맷 옵션 레이블
+lbl_format = Label(frame_option, text="포맷", width=8)
+lbl_format.pack(side="left", padx=5, pady=5)
+# *파일 포맷 옵션 콤보
+opt_format = ["PNG", "JPG", "BMP"]
+cmb_format = ttk.Combobox(frame_option, state="readonly", values=opt_format, width=10)
+cmb_format.current(0)
+cmb_format.pack(side="left", padx=5, pady=5)
+
+# 진행 상황 Bar 프레임
+frame_progress = LabelFrame(root, text="진행 상황")
+frame_progress.pack(fill="x", padx=5, pady=5, ipady=5)
+
+p_var = DoubleVar()
+progress_bar = ttk.Progressbar(frame_progress, maximum=100, variable=p_var)
+progress_bar.pack(fill="x", padx=5, pady=5)
+
+# 실행 프레임
+frame_run = Frame(root)
+frame_run.pack(fill="x", padx=5, pady=5)
+
+btn_close = Button(frame_run, padx=5, pady=5, text="종료", width=12, command=root.quit)
+btn_close.pack(side="right", padx=5, pady=5)
+
+btn_start = Button(frame_run, padx=5, pady=5, text="실행", width=12, command=start)
+btn_start.pack(side="right", padx=5, pady=5)
+
+
+root.resizable(False, False) # X(너비), Y(높이) 값 변경 불가
+root.mainloop()
